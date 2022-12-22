914c5a07319c48e412d1a5eadd85a2459045d778
Jaures00 <jauresdkp@gmail.com>
2022-12-22 18:28:06 +0100

    tout est presque achevé

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,5 @@
 var ibars=document.getElementById("icon-bars");
+var closeicons = document.getElementById("close-icons");
 var sidebar= document.getElementById("side-bar");
 var logo=document.getElementById("Tera")
 var cright=document.getElementById("card-right")
@@ -6,6 +7,7 @@ var divinfo=document.getElementById("info-card")
 var aftnav=document.getElementById("after-nav")
 var t=document.querySelector(".card-edit-tech")
 var l=document.querySelector(".card-edit-lang")
+var p=document.getElementById("point")
 var radios = document.querySelectorAll("input[type='radio']")
 function is_null(params) {
     if (params == null) {
@@ -18,7 +20,8 @@ function SideToogle() {
 
  (!is_null(sidebar)) ? sidebar.classList.toggle("hidden") : '';
  (!is_null(logo)) ? logo.classList.toggle("lg:hidden") : '';
- (!is_null(cright)) ? cright.classList.toggle("lg:w-[80%]") : ''
+ (!is_null(cright)) ? cright.classList.toggle("lg:w-[80%]") : '';
+ (!is_null(cright)) ? cright.classList.toggle("lg:w-full") : '';
  (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-3/4") : '';
  (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-full") : '';
 }
@@ -36,4 +39,52 @@ else if (radios[1].checked) {
     l.classList.toggle('hidden')
 }
 
+}
+document.onclick = check;
+function check(e) {
+  var target = (e && e.target) || (event && event.srcElement);
+
+  //Nav Menu
+  if (!checkParent(target, sidebar)) {
+    // click NOT on the menu
+    console.log(target);
+    if (checkParent(target, ibars)) {
+      (!is_null(sidebar)) ? sidebar.classList.toggle("hidden") : '';
+      (!is_null(logo)) ? logo.classList.toggle("lg:hidden") : '';
+      (!is_null(cright)) ? cright.classList.toggle("lg:w-[80%]") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-3/4") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-full") : '';
+    } 
+    else {
+      // click both outside link and outside menu, hide menu
+      sidebar.classList.add("hidden");
+      (!is_null(logo)) ? logo.classList.toggle("lg:hidden") : '';
+      (!is_null(cright)) ? cright.classList.toggle("lg:w-[80%]") : '';
+      (!is_null(cright)) ? cright.classList.toggle("lg:w-full") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-3/4") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-full") : '';
+    }
+  }
+  if (checkParent(target, p )) {
+    aftnav.classList.toggle("hidden");
+  } 
+  else{ 
+    aftnav.classList.toggle("hidden");
+   }
+  if(checkParent(target, closeicons)) {
+    (!is_null(sidebar)) ? sidebar.classList.toggle("hidden") : '';
+    (!is_null(logo)) ? logo.classList.toggle("lg:hidden") : '';
+      (!is_null(cright)) ? cright.classList.toggle("lg:w-[80%]") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-3/4") : '';
+      (!is_null(divinfo)) ? divinfo.classList.toggle("lg:w-full") : '';
+   }
+}
+function checkParent(t, elm) {
+  while (t.parentNode) {
+    if (t == elm) {
+      return true;
+    }
+    t = t.parentNode;
+  }
+  return false;
 }
